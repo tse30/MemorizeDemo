2a6af2d14503c7708da37ed5f875f063fe5e171d
Jack <jack@example.com>
2023-10-10 15:12:05 +0700

    Initial Commit

---
+++ Memorize Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize Demo
+//
+//  Created by Class Demo on 10/10/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize Demo/Memorize_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_DemoApp.swift
+//  Memorize Demo
+//
+//  Created by Class Demo on 10/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
